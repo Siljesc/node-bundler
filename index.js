9a41712c202ba74bbe51fd7a0617cca8abf408fb
SunriseM <sunrisem.js@gmail.com>
2018-05-12 21:39:59 -0400

    Added ignore file option

--- index.js
+++ index.js
@@ -3,15 +3,22 @@
 const program = require('commander');
 const Bundler = require('./bundler');
 
+function list(val) {
+    return val.split(',');
+}
+
 program
 	.version('0.0.1')
 	.usage('<file> [options]')
     .option('-o, --output <file>', 'output file')
     .option('-v, --verbose', 'enable verbose')
     .option('-b, --beautify', 'beautify output')
+    .option('-i, --ignore <files> example: bundle index.js -i file1.js,lib/file2.js', 'ignore files', list)
     .option('-j, --ignoreJSON', 'ignore json files')
 
 program.parse(process.argv);
+
+console.log(program.ignore);
 
 if (program.args.length === 0) program.help()
 
@@ -20,6 +27,7 @@ const b = new Bundler({
     outputFile: program.output,
     verbose: program.verbose,
     beautify: program.beautify,
+    ignoreFiles: program.ignore,
     disableJSON: program.ignoreJSON
 });
 
